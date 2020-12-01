801b33cd20b19700ebd7275a6e4131c737e52839
fernandof <51677207+fernandof14@users.noreply.github.com>
2020-12-01 09:53:49 -0300

    Add files via upload

--- Taller02UnitTesting.java
+++ Taller02UnitTesting.java
@@ -1,25 +1,35 @@
+import java.text.DecimalFormat;
+
 public class Taller02UnitTesting {
-    int dias=3;
-    static double [][]sismos=new int[][];
-
-
-    public void crearArreglo(){
-        for (int i=0;sismos.length<i;i++){
-            for (int j=0;sismos[i].length<j;j++){
-                sismos[i][j]= Math.random()*10;
-            }
-        }
-    }
-    void imprimirSismos(){
-
-        for (int i=0;sismos.length<i;i++){
-            for (int j=0;sismos[i].length<j;j++){
-                sismos[i][j]= Math.random()*10;
-            }
-        }
-    }
 
     public static void main (String[] args){
+        float [][]sismos= new float[3][8];
+        crearArreglo(sismos);
+        imprimirSismos(sismos);
+
+
+
 
     }
+
+
+    static void crearArreglo(float [][]sismos){
+        for (int i=0;i<sismos.length;i++){
+            for (int j=0;j<sismos[i].length;j++){
+                sismos[i][j]= (float)Math.random()*10;
+            }
+        }
+    }
+    static void imprimirSismos(float [][]sismos){
+        DecimalFormat df = new DecimalFormat("#.00");
+        for (int i=0;i<sismos.length;i++){
+            for (int j=0;j<sismos[i].length;j++){
+                //System.out.printf("["+df.format(sismos[i][j])+"]");
+                System.out.print("["+Math.round(sismos[i][j] * 10d) / 10d+"]");
+            }System.out.println();
+        }
+
+    }
+
+
 }
